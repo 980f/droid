fc090bebc2477a4e82d39d7a1bc78423fa8d19ec
andyh <andy.980f@gmail.com>
2017-01-02 12:16:06 -0600

    reduced as far as is convenient

--- android/src/main/kotlin/pers/hal42/android/GridManager.kt
+++ android/src/main/kotlin/pers/hal42/android/GridManager.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.text.InputType.*
 import android.view.Gravity
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.GridLayout
 
@@ -12,10 +11,8 @@ import android.widget.GridLayout
  * Copyright andyh  10/17/12 3:55 PM
  */
 class GridManager(context: Context) : GridLayout(context) {
-  val dbg: Logger= Logger(GridManager::class.simpleName)
-  companion object {
-    var fixBug = false
-  }
+  val dbg: Logger = Logger(GridManager::class.simpleName)
+  val fixBug = false
 
   /**
    * grid position generator.
@@ -34,7 +31,6 @@ class GridManager(context: Context) : GridLayout(context) {
      * *
      * @return layout for given span, except if <0 or would exceed end of line then remaining on line
      */
-    //    @JvmOverloads
     fun next(span: Int = 1): GridLayout.LayoutParams {
       val columnCount = grid.columnCount
       if (col >= columnCount) { //wrap line
@@ -51,13 +47,6 @@ class GridManager(context: Context) : GridLayout(context) {
       val layout = GridLayout.LayoutParams(GridLayout.spec(row), GridLayout.spec(col, spanish))
       col += spanish
       return layout
-    }
-
-    /**
-     * @return layout of remaining cells in this row
-     */
-    fun eol(): GridLayout.LayoutParams {
-      return next(-1)
     }
 
   }
@@ -105,70 +94,26 @@ class GridManager(context: Context) : GridLayout(context) {
     var typecode = TYPE_CLASS_NUMBER   //x2
     var image = initialValue.toString()
 
-    dbg.i("\n starting image <%s> as %x",image,typecode)
+    dbg.i("\n starting image <%s> as %x", image, typecode)
 
     if (fixBug) {
       if (asInteger) {//must clip trailing .0 or the EditText widget adds a trailing 0 to the returned number
         val clipat = image.indexOfFirst { it == '.' }
         if (clipat >= 0) {//todo: see what 0..-1 does in slice
           image = image.slice(0..clipat - 1)  //
-          dbg.i(" clipped to <%s>",image)
+          dbg.i(" clipped to <%s>", image)
         }
       } else {
         typecode += TYPE_NUMBER_FLAG_DECIMAL   //x2000
       }
+      if (hasSign) {
+        typecode += TYPE_NUMBER_FLAG_SIGNED   //x1000
+      }
     }
-    if (hasSign) {
-      typecode += TYPE_NUMBER_FLAG_SIGNED   //x1000
-    }
-    dbg.i(" typecode %x",typecode)
+    dbg.i(" typecode %x", typecode)  //x0002 when buggy, x2002 when fixed
     editor.inputType = typecode
     editor.setText(image)
     return editor
-  }
-
-  fun addButton(legend: CharSequence, span: Int = 1): Button {
-    val button = add(Button::class.java, span, true)//true: typically want buttons to be as big as possible for fat fingers.
-    button.text = legend
-    return button
-  }
-
-//  fun addButton(legend: CharSequence, action: View.OnClickListener): Button {
-//    return addButton(legend, 1, action)
-//  }
-//
-//  fun addButton(legend: CharSequence, span: Int, action: View.OnClickListener?): Button {
-//    val button = addButton(legend, span)
-//    if (action != null) {
-//      button.setOnClickListener(action)
-//    }
-//    return button
-//  }
-//
-
-  /** trying to do this with just functionals was annoying*/
-  class ToggleButton(context: Context, val whenOn:CharSequence, val whenOff:CharSequence, val action:(doit:Boolean)->Boolean) : Button(context) {
-    init {
-      setOnClickListener( { toggle()})
-      updateLegend(action(false))
-    }
-
-    fun toggle() {
-      val newstate=action(true) //invoke toggle, by using a return value we allow the toggler to veto the toggling.
-      updateLegend(newstate)
-    }
-
-    private fun updateLegend(newstate: Boolean) {
-      text = if (newstate) whenOn else whenOff
-    }
-
-  }
-
-  /** @param action if sent a 1 to actually toggle, a 0 to read the present state, on toggle must return the new value of the state*/
-  fun addToggle(whenOn:CharSequence,whenOff:CharSequence,span:Int=1,wide:Boolean=false,action:(doit:Boolean)->Boolean):ToggleButton {
-    val button = ToggleButton(context, whenOn, whenOff, action)
-    add(button,span,wide)
-    return button
   }
 
 }
--- app/src/main/kotlin/pers/hal42/droid/DroidSampler.kt
+++ app/src/main/kotlin/pers/hal42/droid/DroidSampler.kt
@@ -6,7 +6,11 @@ import pers.hal42.android.*
 /**
  * This is cut down quite a bit from the app I discovered the bug in.
  * I was trying to edit an integer, but found that I was doing a bunch of int<->float conversions which must be explicit in Kotlin.
+ * I didn't know much about the EditText options, "proper" use of them avoids the bug, but it is a bug nonetheless.
  *
+ * Unfixed, when you give a string image of a number that has a decimal point in it to the EditText it initially displays the string as received,
+ * but on first contact with it removes the dp visually, concatenating (in the display) the fractional digits with the integer ones.
+ * After editing which doesn't erase all of the string the value returned on DONE has extra digits in it, extra meaning not shown on the screen.
  * */
 
 class DroidSampler : EasyActivity(2) {
@@ -19,34 +23,16 @@ class DroidSampler : EasyActivity(2) {
 
   internal val editorConnection = NumberEditor.Connection("Edit Value", "set red time (seconds)", false, false, { red.toFloat() }, { value -> red = value.toInt() })
 
-  fun toggleFloating(doit: Boolean): Boolean {
-    if (doit) {
-      myView.printf("\neditorConnection %s",editorConnection.toString())
-      editorConnection.asInteger = !editorConnection.asInteger
-    }
-    return editorConnection.asInteger
-  }
-
-  fun toggleBuggy(doit: Boolean): Boolean {
-    if (doit) {
-      GridManager.fixBug = !GridManager.fixBug
-    }
-    return GridManager.fixBug
-  }
-
   /** */
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    makeButton("Show Value") { red = red }
+    makeButton("Show Value") { red = red }  //show present value via triggering red.set().
     makeButton("Clear Display") { myView.cls() }
-    gridManager.addToggle("Floating", "Integer") { v -> toggleFloating(v) }
-    gridManager.addToggle("Bug Fixed", "Buggy") { v -> toggleBuggy(v) }
 
-    makeLauncher(editorConnection, -1)
+    makeLauncher(editorConnection, -1) //makes a button which popsup an EditText.
 
     //this reference to myView creates it so must occur in an appropriate place to set its screen position.
-    myView.printf("Click Popup the Editor to test\nToggle the bug to disable the workaround")
-    myView.printf("\nrev 18")
+    myView.printf("Click Popup the Editor to test\nSee GridManager fixBug member to enable workaround")
+    myView.printf("\nrev 19") //making sure I actually downloaded an update.
   }
-  internal val dbg= Logger(localClassName)
 }
